0f984a0d9da17e01809b0975e654a26c628cdb69
Pedro <pedrohenriquecordeiro555@gmail.com>
2024-11-08 22:55:11 -0300

    Adição do debug

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ def pesquisa():
     return render_template("pesquisa.html", resultados=resultados, termo_pesquisa=termo_pesquisa)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORTA", 5000)))
+
